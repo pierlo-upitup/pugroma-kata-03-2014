f8a745c8239862da2cc5dee10de5a779cbe22579
Pierluigi Cau <pierlooqup@gmail.com>
2014-03-25 19:48:48 +0100

    Primo test: printRange stampa un intervallo di numeri

--- src/PUGRoma/Kata/Kata.php
+++ src/PUGRoma/Kata/Kata.php
@@ -4,18 +4,29 @@ namespace PUGRoma\Kata;
 
 class Kata
 {
+	private $from;
+
+	private $to;
+
+	public function __constructor($from, $to) 
+	{
+		$this->from = $from;
+		$this->to = $to;
+	}
+
     public function isReady()
     {
         return true;
     }
 
-	public function printRange($from, $to)
+	public function printRange()
 	{
 		$output = '';
-		for ($i = $from; $i <= $to; $i++) 
+		for ($i = $this->from; $i <= $this->to; $i++) 
 		{
 			$output.= $i." ";
 		}
+		
 		return $output;
 	}
 }
--- src/PUGRoma/Tests/KataTest.php
+++ src/PUGRoma/Tests/KataTest.php
@@ -8,17 +8,18 @@ class KataTest extends \PHPUnit_Framework_TestCase
 
 	public function setUp()
 	{
-		$this->kata = new Kata();
+		
 	}
 
     public function testStart()
     {
-    
+    	$this->kata = new Kata(1,1);
         $this->assertTrue($this->kata->isReady());
     }
 
     public function testItPrintsFrom1To10()
     {
+    	$this->kata = new Kata(1,10);
     	$expected = '1 2 3 4 5 6 7 8 9 10 ';
     	$this->assertEquals($expected, $this->kata->printRange(1,10));
     }
