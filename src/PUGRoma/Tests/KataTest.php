bf88f18aaf2bad358a20c8379ddeda5636fb96f1
Pierluigi Cau <pierlooqup@gmail.com>
2014-03-25 19:39:57 +0100

    refactor

--- src/PUGRoma/Tests/KataTest.php
+++ src/PUGRoma/Tests/KataTest.php
@@ -4,9 +4,21 @@ namespace PUGRoma\Kata;
 
 class KataTest extends \PHPUnit_Framework_TestCase
 {
+	private $kata;
+
+	public funcion setUp()
+	{
+		$this->kata = new Kata();
+	}
+
     public function testStart()
     {
-        $kata = new Kata();
-        $this->assertTrue($kata->isReady());
+    
+        $this->assertTrue($this->kata->isReady());
+    }
+
+    public function test_it_prints_from_1_to_100()
+    {
+    	return false;
     }
 }
