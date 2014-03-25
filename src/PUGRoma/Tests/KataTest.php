64e90b10ec261fc4982fed837689a32444b02628
Pierluigi Cau <pierlooqup@gmail.com>
2014-03-25 19:56:18 +0100

    Primo test: corretto a 1 a 15

--- src/PUGRoma/Tests/KataTest.php
+++ src/PUGRoma/Tests/KataTest.php
@@ -17,4 +17,11 @@ class KataTest extends \PHPUnit_Framework_TestCase
     	$expected = '1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz ';
     	$this->assertEquals($expected, $kata->printRange());
     }
+
+    public function testItPrintsFrom1To15()
+    {
+    	$kata = new Kata(1,15);
+    	$expected = '1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz? ';
+    	$this->assertEquals($expected, $kata->printRange());
+    }
 }
